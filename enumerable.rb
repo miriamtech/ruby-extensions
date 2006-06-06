141742370f1cab67e205174eae525b305c5ecc5d
kdt <kdt@3c2adaed-7714-0410-ade1-854bc45bc63c>
2006-06-06 15:54:43 +0000

    Added Enumerable.catalog
    git-svn-id: svn+ssh://miriamtech.repositoryhosting.com/miriamtech/ruby-extensions@6 3c2adaed-7714-0410-ade1-854bc45bc63c

--- enumerable.rb
+++ enumerable.rb
@@ -1,4 +1,9 @@
 module Enumerable
+	def catalog(&block)
+		hashBuilder = self.collect { |each| [each, yield(each)] }.flatten 
+		Hash[*hashBuilder]
+	end
+	
 	def sum
 		self.inject(0) { |sum, each| sum + (block_given? ? yield(each) : each) }
 	end
--- test/enumerable_test.rb
+++ test/enumerable_test.rb
@@ -2,6 +2,13 @@ require 'test/unit'
 require File.dirname(__FILE__) + "/../enumerable.rb"
 
 class EnumerableTest < Test::Unit::TestCase
+	def test_catalog
+		result = ["A", "BCD", "EFGHIJK"].catalog { |each| each.size }
+		assert_equal 1, result["A"]
+		assert_equal 3, result["BCD"]
+		assert_equal 7, result["EFGHIJK"]
+	end
+	
 	def test_sum
 		assert_equal 38, [1, 5, 9, 23].sum
 		assert_equal 10, (1..4).sum
