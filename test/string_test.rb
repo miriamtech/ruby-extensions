624ba6a33535d25cc56399549bd9342d27708749
alk <alk@3c2adaed-7714-0410-ade1-854bc45bc63c>
2007-03-19 16:48:12 +0000

    Added a test for the wordWrap and characterWrap methods.
    git-svn-id: svn+ssh://miriamtech.repositoryhosting.com/miriamtech/ruby-extensions@15 3c2adaed-7714-0410-ade1-854bc45bc63c

--- test/string_test.rb
+++ test/string_test.rb
@@ -10,4 +10,14 @@ class NumericTest < Test::Unit::TestCase
 		# Four byte
 		assert_equal '&#66560;', "\360\220\220\200".xmlEscapeUTF8
 	end
+	def test_characterWrap
+    assert_equal "cha\nrac\nter\ns", 'characters'.characterWrap(3)
+    assert_equal 'abc', 'abc'.characterWrap(3)
+    assert_equal 'abc', 'abc'.characterWrap(5)
+  end
+  
+  def test_wordWrap
+    assert_equal "aaa\nbbbb\ncc", 'aaa bbbb cc'.wordWrap(4)
+    assert_equal "a\nfello\nw\nnamed\nantho\nny\nstrug\ngles\nto\nprogr\nam", "a fellow named anthony struggles to program".wordWrap(5)
+  end
 end
